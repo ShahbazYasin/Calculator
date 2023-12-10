33fa8cd9f3d8f93a9315fe5cd51cc62f8076bb32
shahbaz yasin <shahbazyasin26@gmail.com>
2023-12-10 09:34:59 -0800

    Calculator

---
+++ function.js
@@ -0,0 +1,27 @@
+
+
+function addToInput(value) {
+    let value1 = value
+    document.getElementById('value1').value += value;
+
+}
+
+function allClear() {
+    document.getElementById('inputResult').value = '';
+}
+
+function oneDigitClear() {
+    inputResult.value = inputResult.value.slice(0, -1);
+}
+
+function actionToInput(operator) {
+    var value1 = 
+    var value2 = 
+}
+
+
+
+   
+
+
+  
